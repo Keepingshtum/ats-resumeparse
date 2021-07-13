a8a582a26f84a89602d828568b5b547e56a93f4c
Keepingshtum <anantshukla16@gmail.com>
2021-07-13 20:50:59 +0530

    add file validation logic

--- app/app/views.py
+++ app/app/views.py
@@ -8,10 +8,18 @@ import os
 from flask import render_template,request,redirect,send_from_directory,flash,url_for
 
 UPLOAD_FOLDER = os.getcwd()+"/uploads"
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'docx','zip'}
+
 test = "./"
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.secret_key = "yeet"
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 
 @app.route("/",methods=['GET','POST'])
 def index():
@@ -26,6 +34,9 @@ def index():
             print(file,filename)
             if file.filename =="":
                 flash("No file selected")
+                return redirect(request.url)
+            if not allowed_file(file.filename):
+                flash("Invalid file format!")
                 return redirect(request.url)
             file.save(os.path.join(app.config["UPLOAD_FOLDER"],filename))
             flash("File upload successful!")
